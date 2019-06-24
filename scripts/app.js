bd830356902668b68e1d0904af7531d86eaa5249
github username <danasevcik@hotmail.com>
2019-06-24 19:43:46 -0400

    completed carousel

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+function rotateImages() {
+
+  let image = document.querySelector('.image')
+  let counter = 0
+
+  setInterval(() => {
+      image.style.backgroundImage = `url(./images/hero-image-${counter}.jpg)`
+      counter ++
+      if (counter >= 3) {
+        counter = 0
+      }
+    }, 1000)
+}
+
+
+rotateImages()
